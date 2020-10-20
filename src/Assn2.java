18caa34c5622229eed27d80a504db03a5a6f2894
Spencer Kasteler <sdkasteler@gmail.com>
2020-10-20 12:40:02 -0600

    moved the functionallity out of the main class so the shell is different then the 'wrapper'

--- src/Assn2.java
+++ src/Assn2.java
@@ -1,64 +1,6 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.nio.file.Files;
-
 public class Assn2{
-	static String Directory = "";
 	public static void main(String[] args) throws IOException{
-		Directory = System.getProperty("user.dir"); //get the starting user directory. all directory changes will be here
-		Scanner inputStream = new Scanner(System.in);
-		String inputString = "";
-		while(true){ //the main shell loop
-			System.out.format("[%s]: ", Directory); //display the current dir
-			try{
-				inputString = inputStream.nextLine(); //read a line of input
-			} catch (Exception e){
-				System.exit(0);
-			}
-			String[] parsed = parse(inputString);
-			execute(parsed);
-		}
-	}
-
-	public static void execute(String[] parsed){
-		if(parsed.length > 0){ //used to make sure the user did not input an empty command. without this an empty command throws exception
-			switch(parsed[0]){
-				case "here":
-					Builtins.here(Directory);
-					break;
-				case "exit":
-					System.exit(0);
-					break;
-				case "list":
-					Builtins.list(Directory);
-					break;
-				case "cd":
-					Directory = Builtins.cd(Directory, parsed[1]);
-					break;
-			}
-		}
-
-	}
-	public static String[] parse(String command){
-		java.util.List<String> matchList = new java.util.ArrayList<>();
-		Pattern regex = Pattern.compile("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'");
-		Matcher regexMatcher = regex.matcher(command);
-		while (regexMatcher.find()) {
-			if (regexMatcher.group(1) != null) {
-			// Add double-quoted string without the quotes
-				matchList.add(regexMatcher.group(1));
-			} else if (regexMatcher.group(2) != null) {
-			// Add single-quoted string without the quotes
-				matchList.add(regexMatcher.group(2));
-			} else {
-			// Add unquoted word
-				matchList.add(regexMatcher.group());
-			}
-		}
-		return matchList.toArray(new String[matchList.size()]);
+		Shell s = new Shell(args);
 	}
 }
